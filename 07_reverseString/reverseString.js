e6036bdaa8ba2b5eccbc18bce85681ac279eaf98
Ace Manuyag <119895692+aceytok@users.noreply.github.com>
2025-08-26 21:08:57 -0600

    Add javascript to exercises 06 to 11

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    return word
+            .split('')
+            .reverse()
+            .join('');
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let total = 0;
+    let start = Math.min(a, b);
+    let end = Math.max(a, b);
+    if (a < 0 || b < 0) {
+        return 'ERROR';
+    }
 
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+}
+
+    for (let i = start; i<= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
