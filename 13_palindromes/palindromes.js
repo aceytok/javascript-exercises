3b12164d4ca95c50827ff565625d447f1437360b
Ace Manuyag <119895692+aceytok@users.noreply.github.com>
2025-09-02 16:20:33 -0600

    Add javascript solution to exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let result = string.toLowerCase()
+                    .replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_{|}~]/g, "")
+                    .replace(/\s/g, '')
+    let reversed = result.split('')
+                    .reverse()
+                    .join('')
+    return result === reversed;
 };
 
+let test = palindromes('racecar!')
+
+console.log(test);
 // Do not edit below this line
 module.exports = palindromes;
