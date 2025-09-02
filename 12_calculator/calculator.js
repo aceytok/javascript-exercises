a2d46158d427e1892a13eace4716e722a180cdaa
Ace Manuyag <119895692+aceytok@users.noreply.github.com>
2025-09-02 15:49:31 -0600

    Add javascript solution to exercise 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  let difference = a - b;
+  return difference;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let result = arr.reduce((total, current) => total + current, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr.reduce((total, current) => total * current);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let result = a**b;
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let product = 1;
+  for (let i = a; i > 0; i--) {
+    product *= i;
+  };
+  return product;
 };
 
 // Do not edit below this line
